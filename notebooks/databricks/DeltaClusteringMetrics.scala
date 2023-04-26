2d684d6dd87afff73db7e3f1526de3c561beef1c
Houssem Abidi <abidi.houssem@esprit.tn>
2023-04-26 20:34:00 +0200

    updated the notebook

--- notebooks/databricks/DeltaClusteringMetrics.scala
+++ notebooks/databricks/DeltaClusteringMetrics.scala
@@ -1,6 +1,8 @@
 // Databricks notebook source
 import com.databricks.sql.transaction.tahoe.DeltaLog
-
+import fr.databeans.lighthouse.metrics.delta.DeltaClusteringMetricsBase
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types.StructType
 case class DeltaClusteringMetrics(deltaLog: DeltaLog, spark: SparkSession) extends DeltaClusteringMetricsBase(spark) {
 
   override def schema: StructType = deltaLog.snapshot.schema
@@ -27,6 +29,11 @@ object DeltaClusteringMetrics {
     DeltaClusteringMetrics(deltaLog, spark)
   }
 }
+
+
+
+
+
 
 
 
